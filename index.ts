675ce2428a7f3742ea46bf5e2d5696f1915da47c
Lahiru Sandaruwan <sandaruwanlahiru134@gmail.com>
2025-07-22 23:03:19 +0530

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+
+let cachedServer;
+
+async function bootstrapServer() {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule);
+    await app.init();
+    const expressApp = app.getHttpAdapter().getInstance();
+    cachedServer = expressApp;
+  }
+  return cachedServer;
+}
+
+export default async function handler(req: any, res: any) {
+  const server = await bootstrapServer();
+  return server(req, res);
+}
